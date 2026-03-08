aaa514e7fadcf4749123986718ed87f8c204ad91
AndersonBillss <anderson@thebillsfamily.com>
2026-03-07 17:07:32 -0700

    Implement MySqlAuthDAO

--- server/src/main/java/dataaccess/MySqlAuthDAO.java
+++ server/src/main/java/dataaccess/MySqlAuthDAO.java
@@ -3,6 +3,7 @@ package dataaccess;
 import model.AuthData;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class MySqlAuthDAO implements AuthDAO {
@@ -12,22 +13,68 @@ public class MySqlAuthDAO implements AuthDAO {
 
     @Override
     public void createAuth(AuthData data) throws DataAccessException {
-
+        var statement = "INSERT INTO auth (authToken, username) VALUES (?, ?);";
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1, data.authToken());
+                ps.setString(2, data.username());
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(
+                    String.format("Unable to create auth: %s%n", e.getMessage())
+            );
+        }
     }
 
     @Override
     public AuthData getAuth(String token) throws DataAccessException {
-        return null;
+        var statement = "SELECT authToken, username FROM auth WHERE authToken=?;";
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1, token);
+                var resultSet = ps.executeQuery();
+                if (!resultSet.next()) {
+                    return null;
+                }
+                var resultAuthToken = resultSet.getString(1);
+                var resultUsername = resultSet.getString(2);
+                return new AuthData(resultAuthToken, resultUsername);
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(
+                    String.format("Unable to get auth: %s%n", e.getMessage())
+            );
+        }
     }
 
     @Override
     public void deleteAuth(String token) throws DataAccessException {
-
+        var statement = "DELETE FROM auth where authToken=?;";
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1, token);
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(
+                    String.format("Unable to delete auth: %s%n", e.getMessage())
+            );
+        }
     }
 
     @Override
     public void clear() throws DataAccessException {
-
+        var statement = "DELETE FROM auth;";
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(
+                    String.format("Unable to get user: %s%n", e.getMessage())
+            );
+        }
     }
 
     private void createTable() throws DataAccessException {
