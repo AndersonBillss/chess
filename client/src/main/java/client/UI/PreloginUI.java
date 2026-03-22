d7ed68e84fb779fbe3cc124628f4be006e7a2a5f
AndersonBillss <anderson@thebillsfamily.com>
2026-03-21 21:18:33 -0600

    Implement exit in prelogin UI

--- client/src/main/java/client/UI/PreloginUI.java
+++ client/src/main/java/client/UI/PreloginUI.java
@@ -1,8 +1,42 @@
 package client.UI;
 
-public class PreloginUI implements UI{
+import client.PromptManager;
+import server.ServerFacade;
+
+public class PreloginUI implements UI {
+
+    private ServerFacade facade;
+
+    public PreloginUI(ServerFacade facade, PromptManager manager) {
+        this.facade = facade;
+    }
+
     @Override
     public UI takeInput(String input) {
+        return switch (input.toLowerCase()) {
+            case "help" -> help();
+            case "quit" -> quit();
+            case "login" -> login();
+            case "register" -> register();
+            default -> this;
+        };
+    }
+
+    private UI help() {
+        return this;
+    }
+
+    private UI quit() {
+        System.out.println("Goodbye!");
+        System.exit(0);
+        return null;
+    }
+
+    private UI login() {
+        return this;
+    }
+
+    private UI register() {
         return this;
     }
 }
