5d227bf17ca0ee86c680eaed624b580fbf73ccc3
AndersonBillss <anderson@thebillsfamily.com>
2026-03-22 01:37:16 -0600

    Print border

--- client/src/main/java/client/UI/GameplayUI.java
+++ client/src/main/java/client/UI/GameplayUI.java
@@ -1,7 +1,9 @@
 package client.UI;
 
+import chess.ChessBoard;
 import model.GameData;
 import server.ServerFacade;
+import ui.EscapeSequences;
 
 public class GameplayUI implements UI {
     public enum Mode {
@@ -19,6 +21,7 @@ public class GameplayUI implements UI {
         this.facade = facade;
         this.mode = mode;
         this.game = game;
+        printBoard();
     }
 
     @Override
@@ -34,5 +37,47 @@ public class GameplayUI implements UI {
     @Override
     public UI handleInput(String[] input) {
         return this;
+    }
+
+    private void printBoard() {
+        String borderColorEsc = EscapeSequences.SET_BG_COLOR_GREEN;
+        String borderTextEsc = EscapeSequences.SET_TEXT_COLOR_BLACK;
+        String borderEsc = String.format("%s%s", borderColorEsc, borderTextEsc);
+        String resetEsc = String.format("%s%s",
+                EscapeSequences.RESET_TEXT_COLOR,
+                EscapeSequences.RESET_BG_COLOR);
+        String borderRow = String.format(
+                "%s%sa%sb%sc%sd%se%sf%sg%sh%s%s",
+                borderEsc,
+                EscapeSequences.EMPTY,
+                EscapeSequences.EMPTY,
+                EscapeSequences.EMPTY,
+                EscapeSequences.EMPTY,
+                EscapeSequences.EMPTY,
+                EscapeSequences.EMPTY,
+                EscapeSequences.EMPTY,
+                EscapeSequences.EMPTY,
+                EscapeSequences.EMPTY,
+                resetEsc
+        );
+        System.out.println(borderRow);
+
+        for (int i = 0; i < 8; i++) {
+            String borderCell = String.format(
+                    "%s %d %s",
+                    borderEsc,
+                    i,
+                    resetEsc
+            );
+            System.out.print(borderCell);
+            for (int j = 1; j < 8; j++) {
+
+            }
+            System.out.println(borderCell);
+        }
+
+        System.out.println(borderRow);
+        System.out.print(EscapeSequences.RESET_TEXT_COLOR);
+        System.out.print(EscapeSequences.RESET_BG_COLOR);
     }
 }
