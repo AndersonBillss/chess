73a395a1b621cda3246ac37f78d6d7e232366484
AndersonBillss <anderson@thebillsfamily.com>
2026-03-21 22:16:20 -0600

    Implemented handleInput for PostLoginUI

--- client/src/main/java/client/UI/PostloginUI.java
+++ client/src/main/java/client/UI/PostloginUI.java
@@ -14,6 +14,39 @@ public class PostloginUI implements UI {
 
     @Override
     public UI handleInput(String[] input) {
+        return switch (input[0].toLowerCase()) {
+            case "help" -> help();
+            case "logout" -> logout();
+            case "create" -> create(input);
+            case "list" -> list(input);
+            case "play" -> play(input);
+            case "observe" -> observe(input);
+            default -> this;
+        };
+    }
+
+    private UI help() {
+        System.out.println("HERE IS SOME HELP");
+        return this;
+    }
+
+    private UI logout() {
+        return this;
+    }
+
+    private UI create(String[] input) {
+        return this;
+    }
+
+    private UI list(String[] input) {
+        return this;
+    }
+
+    private UI play(String[] input) {
+        return this;
+    }
+
+    private UI observe(String[] input) {
         return this;
     }
 }
