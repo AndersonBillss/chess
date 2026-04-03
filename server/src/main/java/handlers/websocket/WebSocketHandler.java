05c9a52fed883c0a382ead42f22fb2bcec2b0317
AndersonBillss <anderson@thebillsfamily.com>
2026-04-02 18:34:08 -0600

    Implemented handleMessage

--- server/src/main/java/handlers/websocket/WebSocketHandler.java
+++ server/src/main/java/handlers/websocket/WebSocketHandler.java
@@ -1,21 +1,53 @@
 package handlers.websocket;
 
+import com.google.gson.Gson;
 import io.javalin.websocket.*;
+import org.eclipse.jetty.websocket.api.Session;
 import org.jetbrains.annotations.NotNull;
+import websocket.commands.UserGameCommand;
+
+import java.util.HashSet;
 
 public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
+    private final HashSet<Session> sessions;
+
+    public WebSocketHandler() {
+        this.sessions = new HashSet<>();
+    }
+
     @Override
-    public void handleClose(@NotNull WsCloseContext wsCloseContext) throws Exception {
+    public void handleClose(@NotNull WsCloseContext ctx) throws Exception {
 
     }
 
     @Override
-    public void handleConnect(@NotNull WsConnectContext wsConnectContext) throws Exception {
-
+    public void handleConnect(@NotNull WsConnectContext ctx) throws Exception {
     }
 
     @Override
-    public void handleMessage(@NotNull WsMessageContext wsMessageContext) throws Exception {
+    public void handleMessage(@NotNull WsMessageContext ctx) throws Exception {
+        UserGameCommand command = new Gson().fromJson(ctx.message(), UserGameCommand.class);
+        switch (command.getCommandType()) {
+            case CONNECT -> connect();
+            case MAKE_MOVE -> makeMove();
+            case LEAVE -> leave();
+            case RESIGN -> resign();
+        }
+    }
+
+    private void connect() {
+
+    }
+
+    private void makeMove() {
+
+    }
+
+    private void leave() {
+
+    }
+
+    private void resign() {
 
     }
 }
